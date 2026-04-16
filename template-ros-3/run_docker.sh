ea8bbf543fe271c0cb182d5b4de2b5a68018ce7d
BjornMagnusM <bjornMagnusM@github.com>
2026-04-16 12:40:47 +0200

    gave up on auto transfer to local computer

--- template-ros-3/run_docker.sh
+++ template-ros-3/run_docker.sh
@@ -5,4 +5,5 @@ DUCKIE_IP=$(getent hosts ${DUCKIE_NAME}.local | awk '{print $1}')
 docker run -it --rm \
   --network host \
   --add-host ${DUCKIE_NAME}.local:$DUCKIE_IP \
+  -v ~/Downloads:/videos \
   dt-ros:latest bash
