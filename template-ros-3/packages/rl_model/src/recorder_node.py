7472f4f58374c23ca60f4471f26675aefb5698cf
BjornMagnusM <bjornMagnusM@github.com>
2026-04-16 14:34:48 +0200

    added input to recorder file so it uses the duckiesname from the bash script

--- template-ros-3/packages/rl_model/src/recorder_node.py
+++ template-ros-3/packages/rl_model/src/recorder_node.py
@@ -3,24 +3,22 @@ import rospy
 from sensor_msgs.msg import CompressedImage
 import cv2
 import numpy as np
+import os
 
 class Recorder:
     def __init__(self):
-        self.out = cv2.VideoWriter(
-            "/data/duckie.mp4",
-            cv2.VideoWriter_fourcc(*"mp4v"),
-            20,
-            (640, 480)
+        duckie_name = os.getenv("DUCKIE_NAME", "duckie")
+        rospy.loginfo(f"the duckiebot's name is {duckie_name}")
+        #Video writer 
+        self.out = cv2.VideoWriter("/data/duckie.mp4", cv2.VideoWriter_fourcc(*"mp4v"), 20, (640, 480)
+        )
+        # Subscribers
+        self.sub_image = rospy.Subscriber(
+            f"/{duckie_name}/camera_node/image/compressed", CompressedImage, self.image_cb, buff_size=10000000, queue_size=1
         )
 
-        rospy.Subscriber(
-            "/duckiebot14/camera_node/image/compressed",
-            CompressedImage,
-            self.cb,
-            queue_size=1
-        )
 
-    def cb(self, msg):
+    def image_cb(self, msg):
         np_arr = np.frombuffer(msg.data, np.uint8)
         img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         self.out.write(img)
