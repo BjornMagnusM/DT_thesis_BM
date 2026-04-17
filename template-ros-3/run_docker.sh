d12e96d08dec1c1dc9690950d294114a093ef5e5
BjornMagnusM <bjornMagnusM@github.com>
2026-04-17 14:49:17 +0200

    changed bach script for rl_model so it automaticly finds all the ip adresses

--- template-ros-3/run_docker.sh
+++ template-ros-3/run_docker.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 DUCKIE_NAME=$1
 DUCKIE_IP=$(getent hosts ${DUCKIE_NAME}.local | awk '{print $1}')
+MY_IP=$(hostname -I | awk '{print $1}')
 
 docker run -it --rm \
   --network host \
   --add-host ${DUCKIE_NAME}.local:$DUCKIE_IP \
   -e DUCKIE_NAME=${DUCKIE_NAME} \
+  -e ROS_IP=$MY_IP \
+  -e ROS_MASTER_URI=http://${DUCKIE_IP}:11311 \
   dt-ros:latest bash
