982882f1539df68aa464e8679da7e5fa8e01deef
BjornMagnusM <bjornMagnusM@github.com>
2026-05-08 12:55:43 +0200

    rerun 20th

--- dt-sim/manual_control.py
+++ dt-sim/manual_control.py
@@ -15,7 +15,7 @@ import pyglet
 from pyglet.window import key
 
 from src.gym_duckietown.envs import DuckietownEnv
-from utils.wrappers import CropResizeWrapper,TimeOptimalRewardV2,LapTerminationWrapperV3
+from utils.wrappers import CropResizeWrapper,TimeOptimalRewardV2,LapTerminationWrapperV3,TimeOptimalReward
 
 # from experiments.utils import save_img
 
@@ -52,7 +52,7 @@ if args.env_name and args.env_name.find("Duckietown") != -1:
 else:
     env = gym.make(args.env_name)
 
-env = TimeOptimalRewardV2(env)
+env = TimeOptimalReward(env)
 env = LapTerminationWrapperV3(env,2000)
 
 env = gym.wrappers.RecordEpisodeStatistics(env)
--- dt-sim/utils/rl_env.py
+++ dt-sim/utils/rl_env.py
@@ -5,8 +5,7 @@ from gym_duckietown.simulator import Simulator
 from gymnasium.wrappers import NormalizeReward
 from utils.wrappers import (
     KinematicActionWrapper, ActionWrapper, ResizeWrapper, 
-    CropResizeWrapper, ImgWrapper, CustomRewardWrapper, DtRewardWrapper,
-    TemporalWrapper,LapTerminationWrapperV3,TimeOptimalReward,TimeOptimalRewardV2
+    CropResizeWrapper, ImgWrapper, CustomRewardWrapper, DtRewardWrapper,LapTerminationWrapperV3,TimeOptimalReward,TimeOptimalRewardV2
 )
 
 class DuckieOvalEnv(Simulator):
@@ -66,7 +65,7 @@ class DuckieOvalEnv(Simulator):
         ##BM added a termination criteria after finishing a lap 
         if lap_termination:
             print("using lap termination")
-            env = LapTerminationWrapperV3(env,max_lap_reward=max_lap_reward)
+            env = LapTerminationWrapperV2(env,max_lap_reward=max_lap_reward)
 
 
 
